929c410acd0bd9799f2f6b7d4b7ca8ab4debc6a6
Antoni Lueddeke <antonilueddeke@gmail.com>
2025-08-24 19:55:02 -0400

    created auth app

--- maple_key_backend/urls.py
+++ maple_key_backend/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include('billing.urls')),
+    path("api/billing/", include('billing.urls')),
+    path("api/auth/", include('auth.urls')),
 ]
