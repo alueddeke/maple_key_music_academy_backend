50536375d3774714285d2e078e966b11db8de123
Antoni Lueddeke <antonilueddeke@gmail.com>
2026-03-15 14:06:41 -0400

    Fix: Add HttpResponse import for paystub download endpoint

--- billing/views.py
+++ billing/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
