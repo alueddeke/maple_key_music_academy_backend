b744c371fd0356db16562c53db66acfef884aa0c
Antoni Lueddeke <antonilueddeke@gmail.com>
2026-05-08 08:37:45 -0400

    fix: WR-02 remove unused csrf_exempt import from views.py

--- custom_auth/views.py
+++ custom_auth/views.py
@@ -5,7 +5,6 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from django.views.decorators.csrf import csrf_exempt
 import requests
 import os
 import logging
