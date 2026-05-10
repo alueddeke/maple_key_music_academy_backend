9a7f536c89cf57b4d0ff347fd31f037a6efee579
Antoni Lueddeke <antonilueddeke@gmail.com>
2026-05-10 15:26:32 -0400

    fix: remove 0040 stub, use production real 0040; correct 0041 dependency

--- billing/migrations/0041_add_trial_lesson_status.py
+++ billing/migrations/0041_add_trial_lesson_status.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('billing', '0040_batchlessonitem_admin_notes_main'),
+        ('billing', '0040_batchlessonitem_admin_notes'),
     ]
 
     operations = [
